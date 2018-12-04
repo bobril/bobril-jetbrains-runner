9c7a46c7668d1a3f66339a0a3f64cf9d9d63548e
Michal Baše <m.base@quadient.com>
2018-12-04 10:44:30 +0100

    Added file filter to create hyperlinks in console

---
+++ src/main/kotlin/configuration/FileFilter.kt
@@ -0,0 +1,61 @@
+package configuration
+
+import com.intellij.execution.filters.Filter
+import com.intellij.execution.filters.HyperlinkInfo
+import com.intellij.execution.filters.OpenFileHyperlinkInfo
+import com.intellij.execution.filters.RegexpFilter
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import java.io.File
+import java.util.regex.Pattern
+
+class FileFilter(private val project: Project) : RegexpFilter(project, CONSOLE_FILTER_REGEXP) {
+    private val filePatter = Pattern.compile("([0-9 a-z_A-Z./]+)\\((\\d+),(\\d+)\\)", Pattern.MULTILINE)
+
+    override fun applyFilter(line: String?, entireLength: Int): Filter.Result? {
+
+        val matcher = filePatter.matcher(line)
+        if (!matcher.find() || line == null) {
+            return null
+        }
+
+        val filePath = matcher.group(1) ?: return null
+        val lineNumber = matcher.group(2)
+
+
+        val columnNumber = matcher.group(3)
+
+        var line1 = 0
+        var column = 0
+        try {
+            line1 = Integer.parseInt(lineNumber)
+            column = Integer.parseInt(columnNumber)
+        } catch (e: NumberFormatException) {
+            // Do nothing, so that line and column will remain at their initial zero values.
+        }
+
+        if (line1 > 0) line1 -= 1
+        if (column > 0) column -= 1
+        // Calculate the offsets relative to the entire text.
+        val highlightStartOffset = entireLength - line.length + matcher.start(1)
+        val highlightEndOffset = highlightStartOffset + filePath.length
+        val info = createOpenFileHyperlink(filePath, line1, column)
+        return Filter.Result(highlightStartOffset, highlightEndOffset, info)
+
+    }
+
+    override fun createOpenFileHyperlink(fileName: String?, line: Int, column: Int): HyperlinkInfo? {
+        if (fileName == null) {
+            return null
+        }
+
+        val file = LocalFileSystem.getInstance().findFileByIoFile(File(project.basePath + "/" + fileName)) ?: return null
+
+        return OpenFileHyperlinkInfo(project, file, line, column)
+    }
+
+    companion object {
+        private const val CONSOLE_FILTER_REGEXP = RegexpFilter.FILE_PATH_MACROS + "\\(" + RegexpFilter.LINE_MACROS + "," + RegexpFilter.COLUMN_MACROS + "\\):"
+
+    }
+}
